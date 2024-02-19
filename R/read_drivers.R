c9042679575de25bdd605487f0687c07117bd408
Jota Saraiva <jpsaraiva2002@gmail.com>
2024-02-19 10:50:09 -0300

    fix check issues, adds docu

--- R/read_drivers.R
+++ R/read_drivers.R
@@ -1,13 +1,21 @@
+#' Scrape licensed drivers data from the Brazilian Ministry of Transport.
+#'
+#' @return A `tibble` dataframe.
+#' @export
+#'
+#' @examples
+#' x <- read_drivers()
+#' @importFrom rlang .data
 read_drivers <- function() {
   urls <- get_drivers_links()
 
   dfs <-
     lapply(urls, read_drivers_url) |>
     purrr::reduce(dplyr::bind_rows) |>
-    dplyr::arrange(ano) |>
+    dplyr::arrange("ano") |>
     dplyr::rename_with(~ paste0("categoria_", .),
-                       -c(uf, sexo, faixa_etaria, total, ano)) |>
-    dplyr::mutate(regiao = get_region(uf), .before = 1)
+                       -c("uf", "sexo", "faixa_etaria", "total", "ano")) |>
+    dplyr::mutate(regiao = get_region(.data$uf), .before = 1)
 
   return(dfs)
 }
--- R/utils.R
+++ R/utils.R
@@ -37,11 +37,11 @@ get_region <- function(acronyms) {
 brazil_states_acronym <- function(df) {
   brazil_states_df <- data.frame(
     State = c(
-      "Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal",
-      "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul",
-      "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí",
-      "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima",
-      "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"
+      "Acre", "Alagoas", "Amap\u00e1", "Amazonas", "Bahia", "Cear\u00e1", "Distrito Federal",
+      "Esp\u00edrito Santo", "Goi\u00e1s", "Maranh\u00e3o", "Mato Grosso", "Mato Grosso do Sul",
+      "Minas Gerais", "Par\u00e1", "Para\u00edba", "Paran\u00e1", "Pernambuco", "Piau\u00ed",
+      "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rond\u00f4nia", "Roraima",
+      "Santa Catarina", "S\u00e3o Paulo", "Sergipe", "Tocantins"
     ),
     Acronym = c(
       "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG",
@@ -60,6 +60,7 @@ brazil_states_acronym <- function(df) {
   return(df)
 }
 
+#' @importFrom rlang .data
 read_drivers_url <- function(url) {
   temp <- tempfile(fileext = ".xlsx")
   utils::download.file(url, temp, mode = "wb", quiet = T)
@@ -79,8 +80,8 @@ read_drivers_url <- function(url) {
         ano = as.integer(stringr::str_extract(url, "[12][0-9]{3}"))
       ) |>
       tidyr::fill("uf", "sexo") |>
-      tidyr::drop_na(faixa_etaria) |>
-      dplyr::mutate(faixa_etaria = forcats::as_factor(faixa_etaria)) |>
+      tidyr::drop_na("faixa_etaria") |>
+      dplyr::mutate(faixa_etaria = forcats::as_factor(.data$faixa_etaria)) |>
       brazil_states_acronym()
   })
 
